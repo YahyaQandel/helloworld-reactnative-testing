3a0dd9532ba4357a95f7d19990083d0caa992237
YahyaQandel <yahya.qandel@gmail.com>
2019-09-10 16:44:16 +0200

    inital commit

---
+++ App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,TextInput,Button} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <Text accessibilityLabel={'headerText'} style={styles.welcome}>Welcome to Appium!</Text>
+        <Text accessibilityLabel={'descriptionText'} style={styles.instructions}>Assert that this text exists</Text>
+        <TextInput
+            accessibilityLabel={'textField'}
+            style={{height: 40}}
+            placeholder="Give me pizza!"
+            onChangeText={(text) => this.setState({text})}
+        />
+        <Text accessibilityLabel={'textRendered'}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
